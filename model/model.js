3a71fb528b92228d6b9f05a0bf22021c16443743
Connor ORourke <orourkeconnor@hotmail.co.uk>
2023-01-19 14:44:00 +0000

    minor refactoring from previous merge

--- model/model.js
+++ model/model.js
@@ -37,14 +37,8 @@ function fetchReviewsByReviewId(id) {
 function fetchCommentsByReviewId(id) {
     const sql = `SELECT * FROM comments WHERE comments.review_id = $1 ORDER BY created_at DESC;`
     return db.query(sql, [id]).then(({rows}) => {
-        if (!rows.length && id < 1 || id > 13) {
-            return Promise.reject({status: 404, msg: "invalid review_id input"})
-        } else if (rows === []){
-            return Promise.reject({status: 200, msg: "Does This Work??"})
-        } else {
         return rows
-        }
-    })
+        })
 }
 
 
