127431b184c9f3226c4ef731317ca991501def65
Connor ORourke <orourkeconnor@hotmail.co.uk>
2023-02-06 10:11:13 +0000

    all cross origin resource sharing

--- app/app.js
+++ app/app.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 
 const {getCategories, getReviews, getReviewsByReviewId, getCommentsByReviewId, postCommentByReviewId, patchReview, getUsers} = require('../controller/controller')
 
-
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.get('/api/categories', getCategories)
